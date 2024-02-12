b8222c90ac4f82ca207052bedf46d86487cf996f
sherroman <SherromanRS@yandex.ru>
2024-02-12 20:18:04 +0300

    add router

--- cats/views.py
+++ cats/views.py
@@ -1,13 +1,53 @@
 # cats/views.py
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import mixins
 
 from .models import Cat, Owner
-from .serializers import CatSerializer, OwnerSerializer
+from .serializers import CatListSerializer, CatSerializer, OwnerSerializer
 
+
+# Собираем вьюсет, который будет уметь изменять или удалять отдельный объект.
+# А ничего больше он уметь не будет.
+class UpdateDeleteViewSet(mixins.UpdateModelMixin, mixins.DestroyModelMixin,
+                          viewsets.GenericViewSet):
+    pass 
+
+
+class CreateRetrieveViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                            viewsets.GenericViewSet):
+    # В теле класса никакой код не нужен! Пустячок, а приятно.
+    pass 
+
+
+class LightCatViewSet(CreateRetrieveViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer 
+    
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+    def get_serializer_class(self):
+        # Если запрошенное действие (action) — получение списка объектов ('list')
+        if self.action == 'list':
+            # ...то применяем CatListSerializer
+            return CatListSerializer
+        # А если запрошенное действие — не 'list', применяем CatSerializer
+        return CatSerializer
+
+    # Пишем метод, а в декораторе разрешим работу со списком объектов
+    # и переопределим URL на более презентабельный
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        # Нужны только последние пять котиков белого цвета
+        cats = Cat.objects.filter(color='White')[:5]
+        # Передадим queryset cats сериализатору 
+        # и разрешим работу со списком объектов
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -3,13 +3,20 @@ from rest_framework.routers import DefaultRouter
 
 from django.urls import include, path
 
-from cats.views import CatViewSet, OwnerViewSet
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
+
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
 router.register('owners', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
